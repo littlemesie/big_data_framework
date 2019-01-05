da8165eeb495fef92c8547b69cb219f2e42cbd4e
littlemesie <ranjw@akulaku.com>
2019-01-05 14:39:54 +0800

    update

--- kafka_client/producer.py
+++ kafka_client/producer.py
@@ -6,15 +6,17 @@ from kafka import KafkaProducer
 """
 class Producer(object):
 
-    def __init__(self, kafka_host, kafka_port, kafka_topic):
+    def __init__(self, kafka_hosts, kafka_topic):
         self.kafka_topic = kafka_topic
         # bootstrap_servers 可以是多台服务器
-        self.producer = KafkaProducer(bootstrap_servers=['{kafka_host}:{kafka_port}'.format(kafka_host=kafka_host,kafka_port=kafka_port)])
-        self.produce()
+        self.producer = KafkaProducer(bootstrap_servers=[kafka_hosts])
+        self.send()
 
-    def produce(self):
-        for i in range(1, 10):
-            message = 'message'.format(i)
-            response = self.producer.send(self.kafka_topic, message.encode('utf-8'))
-            # print(response)
+    def send(self):
+        future = self.producer.send('my_topic', key=b'my_key', value=b'my_value', partition=0)
+        result = future.get(timeout=10)
+        print(result)
+
+if __name__ == '__main__':
+    p = Producer('localhost:9092','my_topic')
 
