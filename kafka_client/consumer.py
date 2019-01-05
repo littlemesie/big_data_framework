b7b4c822fb5f795014cd32245ec7d5b1fd107e5b
littlemesie <ranjw@akulaku.com>
2019-01-05 14:39:54 +0800

    update

--- kafka_client/consumer.py
+++ kafka_client/consumer.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
-from kafka import KafkaProducer
 from kafka import KafkaConsumer
-from kafka.errors import KafkaError
-import json
 """
 消费者模块
 """
 class Consumer(object):
 
-    def __init__(self, kafka_host, kafka_port, kafka_topic, group_id):
-        self.consumer = KafkaConsumer(kafka_topic, group_id=group_id,bootstrap_servers=[str(kafka_host) + ':' + str(kafka_port)])
+    def __init__(self, kafka_hosts, kafka_topic):
+        self.consumers = KafkaConsumer(kafka_topic,bootstrap_servers=[kafka_hosts])
 
     def consume(self):
-        for message in self.consumer:
-            print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                                 message.offset, message.key,
-                                                 message.value))
+        for message in self.consumers:
+            print(message)
+
 if __name__ == '__main__':
-    c = Consumer('localhost',9042,'user-event','user-event-tes')
-    c.consumer()
+    c = Consumer('localhost:9092','my_topic')
+    c.consume()
